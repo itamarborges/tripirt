0e1006dba77cf818f32c92cf642812c8141b2e75
Itamar Borges da Silva Filho <itamarborges@gmail.com>
2017-06-04 01:19:38 -0300

    i18n

---
+++ app/i18n/locales/en.js
@@ -0,0 +1,6 @@
+export default {
+  my_itineraries: 'My Itineraries',
+  search_itineraries: 'Search Itineraries',
+  my_profile: 'My Profile',
+  my_guides: 'My Guides'
+};
---
+++ app/i18n/locales/pt.js
@@ -0,0 +1,6 @@
+export default {
+  my_itineraries: 'Meus Itinerários',
+  search_itineraries: 'Procurar Itinerários',
+  my_profile: 'Meu Perfil',
+  my_guides: 'Guias de Viagem'
+};
